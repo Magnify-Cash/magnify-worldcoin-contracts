62f9cff171587a1f31bea0825049ea789183c4df
Wade <wz.lim.00@gmail.com>
2025-03-07 13:42:33 +0800

    feat: NFT details

--- contracts/MagnifyWorldSoulboundNFT.sol
+++ contracts/MagnifyWorldSoulboundNFT.sol
@@ -10,18 +10,46 @@ contract MagnifyWorldSoulboundNFT is
     OwnableUpgradeable,
     IMagnifyWorldSoulboundNFT
 {
-    mapping(uint256 => NFTData) public repaymentInfo;
+    uint256 internal tokenCount;
+    mapping(uint256 => NFTData) public nftData;
+    mapping(address => uint256) public userToId;
     mapping(address => bool) public admins;
     uint256[50] __gap;
+
+    error AlreadyOwnedNFT();
 
     modifier onlyAdmin() {
         require(admins[msg.sender], "admin: called is not an admin");
         _;
     }
 
-    function initialize() public initializer {}
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __ERC721_init("Magnify World NFT", "MAGNFT");
+    }
 
-    function mintNFT() public onlyAdmin {}
+    function mintNFT(address _to, uint8 _tier) public onlyAdmin {
+        if (userToId[_to] != 0) {
+            revert AlreadyOwnedNFT(); 
+        }
+        tokenCount++;
+        _safeMint(_to, tokenCount);
+        nftData[tokenCount] = NFTData(0, 0, 0, _to, _tier);
+    }
+
+    function upgradeTier(uint256 _tokenId, uint8 _newTier) public onlyAdmin {
+        _requireOwned(_tokenId);
+        nftData[_tokenId].tier = _newTier;
+    }
+
+    function updateloanRepayment(uint256 _tokenId, uint256 _interestPaid) external onlyAdmin {
+        nftData[_tokenId].loansRepaid++;
+        nftData[_tokenId].interestPaid += _interestPaid;
+    }
+
+    function updateLoanDefault(uint256 _tokenId, uint256 _defaultAmount) external onlyAdmin {
+        nftData[_tokenId].loansDefaulted+= _defaultAmount;
+    }
 
     // Set multiple admin wallets
     function setAdmin(address _address, bool _allow) external onlyOwner {
