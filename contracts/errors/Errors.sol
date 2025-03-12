42525ab37f55dd0d7473a30eb0d9c6aedddb66c9
Wade <wz.lim.00@gmail.com>
2025-03-12 11:03:14 +0800

    fix: added errors file

---
+++ contracts/errors/Errors.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+library Errors {
+    
+    // Tier errors
+    error TierExists();
+    error TierNotExists();
+    error TierInsufficient();
+
+    // Loan Errors
+    error LoanActive();
+    error NoLoanActive();
+    error NoLoanDefault();
+    error LoanIdNotFound();
+    error DefaultDetected();
+    error InsufficientLiquidity();
+    error LoanExpired();
+
+    // Repay Error
+    error PermitInvalidToken();
+    error PermitInvalidAmount();
+    error TransferInvalidAmount();
+    error TransferInvalidAddress();
+
+    // NFT Errors
+    error NoMagnifyNFT();
+    error AlreadyOwnedNFT();
+    error TokenIdInvalid(uint256 tokenId);
+
+    error CallerNotAdmin();
+    error InputZero();
+
+}
