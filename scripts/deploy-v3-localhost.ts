7db56457f8b81234c943d1ffa29b2e65f7a9fc15
Wade <wz.lim.00@gmail.com>
2025-03-12 17:32:29 +0800

    feat: added localhost deployment of v3

---
+++ scripts/deploy-v3-localhost.ts
@@ -0,0 +1,126 @@
+import { BaseContract, ContractTransactionResponse } from "ethers";
+import { ethers, upgrades } from "hardhat";
+import { mock } from "node:test";
+import {
+  MagnifyWorldV3,
+  MagnifyWorldSoulboundNFT,
+  MockERC20,
+  MockPermit2,
+} from "../typechain-types";
+
+const NFT_NAME = "Magnify World Soulbound NFT";
+const NFT_SYMBOL = "MAGNFT";
+const NAME = "MagnifyWorldV3";
+const SYMBOL = "MAGV3";
+const OWNER = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const TREASURY_ADDRESS = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+
+// https://docs.openzeppelin.com/contracts/2.x/api/token/erc20#IERC20
+// https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#ERC4626
+
+async function deploy() {
+  const MockToken = await ethers.getContractFactory("MockERC20");
+  const mockToken = await MockToken.deploy("Mock Token", "MTK");
+
+  // Deploy mock Permit2
+  const MockPermit2 = await ethers.getContractFactory("MockPermit2");
+  const mockPermit2 = await MockPermit2.deploy();
+
+  // Get the contract factory
+  const MagnifyWorldV1 = await ethers.getContractFactory(
+    "contracts/MagnifyWorldV1.sol:MagnifyWorld"
+  );
+  const magnifyWorldV1 = await MagnifyWorldV1.deploy(
+    await mockToken.getAddress(),
+    await mockPermit2.getAddress()
+  );
+  await magnifyWorldV1.waitForDeployment();
+
+  // Deploy MagnifyWorldSoulboundNFT contract
+  const MagnifyWorldSoulboundNFT = await ethers.getContractFactory(
+    "MagnifyWorldSoulboundNFT"
+  );
+  const magnifyWorldSoulboundNFT = await upgrades.deployProxy(
+    MagnifyWorldSoulboundNFT,
+    [NFT_NAME, NFT_SYMBOL]
+  );
+  await magnifyWorldSoulboundNFT.waitForDeployment();
+  // Deploy V3 contract
+  const MagnifyWorldV3 = await ethers.getContractFactory("MagnifyWorldV3");
+  const magnifyWorldV3 = await upgrades.deployProxy(MagnifyWorldV3, [
+    NAME,
+    SYMBOL,
+    await mockToken.getAddress(),
+    await mockPermit2.getAddress(),
+    await magnifyWorldSoulboundNFT.getAddress(),
+    await magnifyWorldV1.getAddress(),
+    TREASURY_ADDRESS,
+  ]);
+  await magnifyWorldV3.waitForDeployment();
+
+  console.log("MockToken deployed to:", await mockToken.getAddress());
+  console.log("MockPermit2 deployed to:", await mockPermit2.getAddress());
+  console.log("MagnifyWorldV1 deployed to:", await magnifyWorldV1.getAddress());
+  console.log(
+    "MagnifyWorld SoulboundNFT deployed to:",
+    await magnifyWorldSoulboundNFT.getAddress()
+  );
+  console.log("MagnifyWorldV3 deployed to:", await magnifyWorldV3.getAddress());
+  return {
+    magnifyWorldV1,
+    magnifyWorldV3,
+    magnifyWorldSoulboundNFT,
+    mockToken,
+    mockPermit2,
+  };
+}
+
+async function setup(
+  magnifyWorldV1: BaseContract,
+  magnifyWorldV3: MagnifyWorldV3,
+  magnifyWorldSoulboundNFT: MagnifyWorldSoulboundNFT,
+  mockToken: MockERC20,
+  mockPermit2: MockPermit2
+) {
+  // Mint tokens to user
+  const mintAmount = ethers.parseUnits("1000", 6); // Assuming 6 decimals
+  await mockToken.mint(OWNER, mintAmount);
+
+  // mint owner nft
+  await magnifyWorldSoulboundNFT.mintNFT(OWNER, 99);
+
+  await mockToken.approve(await magnifyWorldV3.getAddress(), mintAmount);
+  await magnifyWorldV3.deposit(mintAmount, OWNER);
+
+  console.log("balance of loan lp:", await magnifyWorldV3.balanceOf(OWNER));
+  console.log("value of loan lp:", await magnifyWorldV3.convertToAssets(await magnifyWorldV3.balanceOf(OWNER)));
+  await mockToken.mint(await magnifyWorldV3.getAddress(), mintAmount);
+  console.log("value of loan lp:", await magnifyWorldV3.convertToAssets(await magnifyWorldV3.balanceOf(OWNER)));
+
+}
+
+async function main() {
+  const {
+    magnifyWorldV1,
+    magnifyWorldV3,
+    magnifyWorldSoulboundNFT,
+    mockToken,
+    mockPermit2,
+  } = await deploy();
+
+  await setup(
+    magnifyWorldV1,
+    magnifyWorldV3,
+    magnifyWorldSoulboundNFT,
+    mockToken,
+    mockPermit2
+  );
+}
+
+// Execute the deployment
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
